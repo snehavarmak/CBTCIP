d20c870a8bd1ffade44c1d5343d3147917ce7971
Sneha <kollusnehalatha27@gmail.com>
2024-08-13 17:39:42 +0000

    Event Planner

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,28 +1,12 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+const {Schema} = mongoose;
 
-const UserSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    password: {
-        type: String,
-        required: true,
-    },
-});
+const UserSchema = new Schema({
+  name: String,
+  email: {type:String, unique:true},
+  password: String
+})
 
-UserSchema.pre('save', async function(next) {
-    if (!this.isModified('password')) {
-        return next();
-    }
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-});
+const UserModel = mongoose.model('User', UserSchema);
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = UserModel;
