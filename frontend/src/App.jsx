5e7584a2ebf346f1021b0beeee00fb99d2c65d47
Sneha <kollusnehalatha27@gmail.com>
2024-08-14 11:43:31 +0000

    Event Planner

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,7 +18,7 @@ import PaymentSummary from './pages/PaymentSummary'
 import TicketPage from './pages/TicketPage'
 import CreatEvent from './pages/CreateEvent'
 
-axios.defaults.baseURL = 'http://localhost:4000/';
+axios.defaults.baseURL = 'https://event-planner-backend-virid.vercel.app/';
 axios.defaults.withCredentials=true;
 
 function App() {
